511f7551f256ed2e40947130e47751f52328e380
Vito Abeln <vito.abeln@gmail.com>
2020-05-18 10:45:21 +0200

    Added favicon

--- header.php
+++ header.php
@@ -11,7 +11,23 @@
    <meta charset="utf-8">
    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
    <title>Iskalnik izdelkov</title>
-   <link rel="icon" href="img/favicon.png">
+   <link rel="apple-touch-icon" sizes="57x57" href="favicon/apple-icon-57x57.png">
+   <link rel="apple-touch-icon" sizes="60x60" href="favicon/apple-icon-60x60.png">
+   <link rel="apple-touch-icon" sizes="72x72" href="favicon/apple-icon-72x72.png">
+   <link rel="apple-touch-icon" sizes="76x76" href="favicon/apple-icon-76x76.png">
+   <link rel="apple-touch-icon" sizes="114x114" href="favicon/apple-icon-114x114.png">
+   <link rel="apple-touch-icon" sizes="120x120" href="favicon/apple-icon-120x120.png">
+   <link rel="apple-touch-icon" sizes="144x144" href="favicon/apple-icon-144x144.png">
+   <link rel="apple-touch-icon" sizes="152x152" href="favicon/apple-icon-152x152.png">
+   <link rel="apple-touch-icon" sizes="180x180" href="favicon/apple-icon-180x180.png">
+   <link rel="icon" type="image/png" sizes="192x192"  href="favicon/android-icon-192x192.png">
+   <link rel="icon" type="image/png" sizes="32x32" href="favicon/favicon-32x32.png">
+   <link rel="icon" type="image/png" sizes="96x96" href="favicon/favicon-96x96.png">
+   <link rel="icon" type="image/png" sizes="16x16" href="favicon/favicon-16x16.png">
+   <link rel="manifest" href="favicon/manifest.json">
+   <meta name="msapplication-TileColor" content="#ffffff">
+   <meta name="msapplication-TileImage" content="/ms-icon-144x144.png">
+   <meta name="theme-color" content="#ffffff">
    <!-- Bootstrap/jQuery/Popper-->
    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">   <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
